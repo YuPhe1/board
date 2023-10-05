d68d0b2fcb29e4e5e14f42bade98eded2a378925
YuPhe1 <oksunboy1@naver.com>
2023-10-05 10:10:44 +0900

    10. 수정 및 삭제 중복 코드 줄이기

--- src/main/java/com/icia/board/controller/BoardController.java
+++ src/main/java/com/icia/board/controller/BoardController.java
@@ -20,66 +20,61 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/save")
-    public String savePage(){
+    public String savePage() {
         return "boardPages/boardSave";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute BoardDTO boardDTO){
+    public String save(@ModelAttribute BoardDTO boardDTO) {
         boardService.save(boardDTO);
         return "redirect:/board";
     }
 
     @GetMapping
-    public String findAll(Model model){
+    public String findAll(Model model) {
         List<BoardDTO> boardDTOList = boardService.findAll();
         model.addAttribute("boardList", boardDTOList);
         return "boardPages/boardList";
     }
 
     @GetMapping("/{id}")
-    public String detail(@PathVariable("id") Long id, Model model){
+    public String detail(@PathVariable("id") Long id, Model model) {
         try {
             boardService.increaseHits(id);
             BoardDTO boardDTO = boardService.findById(id);
             model.addAttribute("board", boardDTO);
             return "boardPages/boardDetail";
-        } catch (NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             return "boardPages/notFound";
         }
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Long id){
+    public String delete(@PathVariable("id") Long id) {
         boardService.delete(id);
         return "redirect:/board";
     }
 
     @GetMapping("/update/{id}")
-    public String updatePage(@PathVariable("id") Long id, Model model){
+    public String updatePage(@PathVariable("id") Long id, Model model) {
         model.addAttribute("board", boardService.findById(id));
         return "boardPages/boardUpdate";
     }
 
     @PostMapping("/update")
-    public String update(@ModelAttribute BoardDTO boardDTO){
+    public String update(@ModelAttribute BoardDTO boardDTO) {
         boardService.update(boardDTO);
         return "redirect:/board/" + boardDTO.getId();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity delete(@PathVariable("id") Long id, @RequestParam("boardPass") String boardPass){
-        BoardDTO boardDTO = boardService.findById(id);
-        if(boardDTO.getBoardPass().equals(boardPass)) {
-            boardService.delete(id);
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        else
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+    public ResponseEntity axiosDelete(@PathVariable("id") Long id) {
+        boardService.delete(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@PathVariable("id") Long id, @RequestBody BoardDTO boardDTO){
+    public ResponseEntity update(@PathVariable("id") Long id, @RequestBody BoardDTO boardDTO) {
         boardService.update(boardDTO);
         return new ResponseEntity(HttpStatus.OK);
     }
