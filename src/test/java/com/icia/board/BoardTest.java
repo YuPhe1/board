164f15ec9c5bb0cda6a78d946456a6808294c57c
YuPhe1 <oksunboy1@naver.com>
2023-10-05 15:38:32 +0900

    12-1. 검색 페이징 repository

--- src/test/java/com/icia/board/BoardTest.java
+++ src/test/java/com/icia/board/BoardTest.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -92,5 +93,40 @@ public class BoardTest {
         Long saveId = boardService.save(boardDTO);
         BoardDTO findBoard = boardService.findById(saveId);
         assertThat(boardDTO.getBoardTitle()).isEqualTo(findBoard.getBoardTitle());
+    }
+
+    @Test
+    @DisplayName("검색 메서드 확인")
+    public void searchMethod(){
+//        List<BoardEntity> boardEntityList = boardRepository.findByBoardTitleContainingOrderById("1");
+//        boardEntityList.forEach(boardEntity -> {
+//            System.out.println(BoardDTO.toDTO(boardEntity));
+//        });
+
+        // 제목에 1이 포홤된 결과 페이징
+        int page = 0;
+        int pageLimit = 5;
+        Page<BoardEntity> boardEntities = boardRepository.findByBoardTitleContaining("1", PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+
+        String q = "1";
+        boardEntities = boardRepository.findByBoardTitleContainingOrBoardWriterContaining(q, q, PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+        Page<BoardDTO> boardList = boardEntities.map(boardEntity ->
+                BoardDTO.builder()
+                        .id(boardEntity.getId())
+                        .boardWriter(boardEntity.getBoardWriter())
+                        .boardTitle(boardEntity.getBoardTitle())
+                        .boardHits(boardEntity.getBoardHits())
+                        .createdAt(UtilClass.dateTimeFormat((boardEntity.getCreatedAt())))
+                        .build());
+
+        System.out.println("boardList.getContent() = " + boardList.getContent()); // 요청페이지에 들어있는 데이터
+        System.out.println("boardList.getTotalElements() = " + boardList.getTotalElements()); // 전체 글갯수
+        System.out.println("boardList.getNumber() = " + boardList.getNumber()); // 요청페이지(jpa 기준)
+        System.out.println("boardList.getTotalPages() = " + boardList.getTotalPages()); // 전체 페이지 갯수
+        System.out.println("boardList.getSize() = " + boardList.getSize()); // 한페이지에 보여지는 글갯수
+        System.out.println("boardList.hasPrevious() = " + boardList.hasPrevious()); // 이전페이지 존재 여부
+        System.out.println("boardList.isFirst() = " + boardList.isFirst()); // 첫페이지인지 여부
+        System.out.println("boardList.isLast() = " + boardList.isLast()); // 마지막페이지인지 여부
+
     }
 }
